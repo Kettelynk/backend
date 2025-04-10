caaaff5fc14cb1160cbba94653818f1fdea98785
kettelynk <kettelyn.m@estudante.ifro.edu.br>
2025-04-10 10:07:47 -0400

    atividades

--- exercicio_01.js
+++ exercicio_01.js
@@ -1,4 +1,5 @@
 let nome = "kettelyn"
 let idade = "17"
 
-console.log("olá, meu nome é", nome, "e eu tenho", idade, "anos")
+console.log("olá, meu nome é"+nome+"e eu tenho"+idade+"anos")// concatenacao
+console.log(`ola, meu nome e ${nome} e eu tenho ${idade} anos.`)
--- exercicio_04.js
+++ exercicio_04.js
@@ -1,5 +1,5 @@
 let altura = 1.60;
 let peso = 66;
 
-let soma = peso/(altura*altura);
-console.log(soma)
+let soma = (peso/(altura*altura)).toFixed(2);
+console.log(`seu imc é: ${soma}`)
