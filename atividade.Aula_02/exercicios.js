e7f6bbe08932aca83211eb52fc3223e5a12aec86
kettelynk <kettelyn.m@estudante.ifro.edu.br>
2025-04-10 11:47:31 -0400

    aula 2

---
+++ atividade.Aula_02/exercicios.js
@@ -0,0 +1,28 @@
+let x = 5
+let y = 5
+
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y);
+
+
+let julia = 16
+let pedro = 12
+
+let msg = julia >= pedro ? 'julia é mais velha que pedro' : 'pedro é mais velho que julia '
+
+console.log(msg)
+
+let idade = 16
+
+m = idade == 18 ? "maior de idade" : "menor de idade"
+console.log(m)
+
+let a = 14
+let soma = a % 2
+let men = a == 0 ? 'impar' : 'par'
+console.log(men)
+ 
+
+
