4ccc04fcfe1f78d4e72044a1da9818aaf34f907e
kettelynk <kettelyn.m@estudante.ifro.edu.br>
2025-04-24 10:13:25 -0400

    correcao da segunda atividade

--- atividade.Aula_02/exercicios.js
+++ atividade.Aula_02/exercicios.js
@@ -1,28 +1,31 @@
+//Questão 1
 let x = 5
 let y = 5
 
-console.log(x + y)
-console.log(x - y)
-console.log(x * y)
-console.log(x / y);
+console.log(`${x} + ${y} = ${x + y}`)
+console.log(`${x} - ${y} = ${x - y}`)
+console.log(`${x} * ${y} = ${x * y}`)
+console.log(`${x} / ${y} = ${x / y}`);
 
-
+//Questão 2
 let julia = 16
 let pedro = 12
-
 let msg = julia >= pedro ? 'julia é mais velha que pedro' : 'pedro é mais velho que julia '
 
 console.log(msg)
 
+//questao 3
 let idade = 16
 
 m = idade == 18 ? "maior de idade" : "menor de idade"
 console.log(m)
 
+//Questão 4
 let a = 14
 let soma = a % 2
 let men = a == 0 ? 'impar' : 'par'
 console.log(men)
- 
+
+
 
 
